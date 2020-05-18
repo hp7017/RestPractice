cb79dab30d2a848eacc794db9a1f923178d0cdb9
hp7017 <himanshupharawal100@gmail.com>
2020-05-18 13:27:53 +0000

    admin filter added

--- LibGen/admin.py
+++ LibGen/admin.py
@@ -3,7 +3,28 @@ from . import models
 
 # Register your models here.
 
+class BookInline(admin.TabularInline):
+	model = models.Book
+
+class SearchInline(admin.TabularInline):
+	model = models.Search
+
+class CuserAdmin(admin.ModelAdmin):
+	readonly_fields = ['created', 'last_login']
+	list_filter = ('status', 'created', 'last_login', 'version')
+	list_display = ('name', 'status', 'last_login')
+	inlines = [SearchInline]
+
+class SearchAdmin(admin.ModelAdmin):
+	list_display = ['search', 'user', 'date']
+	list_filter = ['date']
+	inlines = [BookInline]
+
+class BookAdmin(admin.ModelAdmin):
+	list_display = ['name', 'search', 'date']
+	list_filter = ['date']
+
 admin.site.register(models.App)
-admin.site.register(models.Search)
-admin.site.register(models.CUser)
-admin.site.register(models.Book)
+admin.site.register(models.Search, SearchAdmin)
+admin.site.register(models.CUser, CuserAdmin)
+admin.site.register(models.Book, BookAdmin)
