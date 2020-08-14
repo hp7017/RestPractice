34fd3f6ce95a9a7cba01e8085c45b0071ebc4697
Himanshu Pharawal <himanshupharawal100@gmail.com>
2020-08-14 16:27:58 +0530

    async correctly added for production

--- async_send_emails.py
+++ async_send_emails.py
@@ -3,14 +3,11 @@ import django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'RestPractice.settings'
 django.setup()
 
-from django.core.mail import EmailMessage, get_connection
+from django.core.mail import EmailMessage
 from LibGen import models
 while True:
 	if models.Email.objects.exists():
 		emails = models.Email.objects.all()
-		emails_for_connection = []
-		connection = get_connection()
-		connection.open()
 		for email in emails:
 			email_message = EmailMessage(
 				subject=email.subject,
@@ -19,8 +16,6 @@ while True:
 				to=[email.to_email],
 				reply_to=[email.reply_to]
 			)
-			emails_for_connection.append(email_message)
 			print(f'sent from: {email.from_email}\tto:{[email.to_email]}')
+			email_message.send(fail_silently=False)
 			email.delete()
-		connection.send_messages(emails)
-		connection.close()
